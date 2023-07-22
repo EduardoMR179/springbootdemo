a3406a9076f13ca0fd1b81fafd22401a94849593
EduardoMR179 <eemr0001@ce.pucmm.edu.do>
2023-07-22 04:11:37 -0400

    First Commit

---
+++ src/main/java/in/bushansirgur/demo/HomeController.java
@@ -0,0 +1,12 @@
+package in.bushansirgur.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping({"/"})
+    public String getStatus(){
+        return "Application is up and running";
+    }
+}
